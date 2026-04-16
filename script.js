ea3f6b2db83d7adae3bfadb7511565e378fb1e64
jamieotieno <jamie.otieno@student.moringaschool.com>
2026-04-17 01:03:06 +0300

    add function to add income and expenses

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ async function getTransactions() {
     console.error("Error fetching transactions:", error.message);
   }
 }
+
+// Adding Income and Expenses
+async function addTransaction(type, amount, category) {
+  try {
+    const response = await fetch(`${BASE_URL}/transactions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        type,
+        amount,
+        category,
+        date: new Date().toISOString()
+      })
+    });
+
+    const data = await response.json();
+    console.log("Saved:", data);
+  } catch (error) {
+    console.error("Error saving transaction:", error.message);
+  }
+}
